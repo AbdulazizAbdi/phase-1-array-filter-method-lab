ed72298cdf80c3b52f4f1d1879c71079173ac680
Abdulaziz Abdi <amigf76@gmail.com>
2024-03-27 19:45:41 +0300

    filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(driverNameArray, str){
+    let listOfDriver = driverNameArray.filter(function (checkName) {return checkName.toLowerCase() === str.toLowerCase()});
+    return listOfDriver;
+}
+
+function fuzzyMatch(driverNameArray, str){
+    return driverNameArray.filter(function(checkName){return checkName.slice(0,2) === str.slice(0,2)});
+}
+
+function matchName(driverNameObj, str){
+    return driverNameObj.filter(function(driver){
+        return driver.name === str });
+}
